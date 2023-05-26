b4bdaab0b8dd2a4220cabd52401b2872720ff39a
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-26 17:16:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Function to get the width and height of the window
+ // Function to update the width and height of the window in the webpage
+    function updateWindowSize() {
+      var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+      var windowSizeElement = document.getElementById('windowSize');
+      windowSizeElement.textContent = 'Width: ' + width + 'px, Height: ' + height + 'px';
+    }
+
+    // Initial call to update the window size
+    updateWindowSize();
+
+    // Event listener for window resize
+    window.addEventListener('resize', updateWindowSize);
